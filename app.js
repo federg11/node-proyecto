a7b73fef93cadd94460f758065f3e1df13e9b56a
federg <federicorg07@gmail.com>
2023-08-30 18:07:23 -0300

    [FEAT] initial server config

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+
+app.get('/', (req, res) => res.send('Hola desde NODE'))
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
